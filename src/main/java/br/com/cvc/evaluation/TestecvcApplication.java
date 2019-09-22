bbb8a31ed9b1570579f2ab154aa0f5592cec8cfc
João Paulo BS <ze.kiefa@gmail.com>
2019-09-22 00:08:04 -0300

    Add files via upload

---
+++ src/main/java/br/com/cvc/evaluation/TestecvcApplication.java
@@ -0,0 +1,13 @@
+package br.com.cvc.evaluation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestecvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestecvcApplication.class, args);
+	}
+
+}
