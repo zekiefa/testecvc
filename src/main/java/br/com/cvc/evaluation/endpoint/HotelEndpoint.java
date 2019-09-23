fd3e2dc014cb97e1ee2c766ff2f49b3a8b98a2b5
João Paulo BS <ze.kieifa@gmail.com>
2019-09-23 02:35:18 -0300

    Chamada do serviço de Booking.

--- src/main/java/br/com/cvc/evaluation/endpoint/HotelEndpoint.java
+++ src/main/java/br/com/cvc/evaluation/endpoint/HotelEndpoint.java
@@ -1,20 +1,25 @@
 package br.com.cvc.evaluation.endpoint;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.cvc.evaluation.endpoint.dto.BookingResponse;
+import br.com.cvc.evaluation.domain.Hotel;
+import br.com.cvc.evaluation.service.BookingService;
 
 @RestController
 @RequestMapping("/hotel")
 public class HotelEndpoint {
 
-	@GetMapping("/{id}")
-	public BookingResponse find(@PathVariable("id") final Integer id) {
+	@Autowired
+	private BookingService bookingService;
 
-		return new BookingResponse();
+	@GetMapping("/{id}")
+	public Hotel find(@PathVariable("id") final Integer id) {
+
+		return this.bookingService.getHotelDetails(id);
 	}
 
 }
