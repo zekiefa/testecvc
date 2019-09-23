9a62b86f4a12048643258b61c8a27bf83d95b836
João Paulo BS <ze.kieifa@gmail.com>
2019-09-22 23:18:58 -0300

    Suporte a RestTemplate.

--- src/main/java/br/com/cvc/evaluation/EvaluationApplication.java
+++ src/main/java/br/com/cvc/evaluation/EvaluationApplication.java
@@ -2,14 +2,22 @@ package br.com.cvc.evaluation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-@ComponentScan(value = "br.com.cvc.evaluation.endpoint")
+@ComponentScan(value = { "br.com.cvc.evaluation.endpoint", "br.com.cvc.evaluation.broker" })
 public class EvaluationApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EvaluationApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate restTemplate(final RestTemplateBuilder builder) {
+		return builder.build();
 	}
 
 }
